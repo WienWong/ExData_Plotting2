7919945a019b835f3483c86f34e19850bfaf1d6b
WienWong <wangzichengfu@gmail.com>
2015-01-18 04:11:18 +0100

    Create plot2.R
    1st v

---
+++ plot2.R
@@ -0,0 +1,34 @@
+
+setwd("D:/Coursera_R/exdata-data-NEI_data")
+
+f1 <- readRDS("summarySCC_PM25.rds", refhook = NULL)
+
+f2 <- readRDS("Source_Classification_Code.rds", refhook = NULL)
+
+Baltimore <- f1[f1$fips == "24510", ] 
+
+direct <- paste (getwd(), "/plot2.png", sep = "", collapse = NULL)
+
+png(filename = direct, width = 500, height = 500, units = "px")
+
+totEmi_Ba <- aggregate(Baltimore$Emissions, by = list(Baltimore$year), FUN = "sum", na.rm=TRUE)
+
+head(totEmi_Ba)
+#   Group.1        x
+# 1    1999 3274.180
+# 2    2002 2453.916
+# 3    2005 3091.354
+# 4    2008 1862.282
+
+barplot(totEmi_Ba$x, names.arg = totEmi_Ba$Group.1, main = "Total Emissions at Baltimore City", xlab = "Year", ylab = expression('Total PM'[2.5]*" Emission (Tons)"), ylim = c(0,3500))
+
+# par(new=TRUE)  
+# 
+# plot(totEmi_Ba$x, type = "l", axes=FALSE, xlab = NA, ylab = NA, ylim = c(0,3500), lwd = 3, col = 'blue')
+# 
+# par(new=TRUE)
+# 
+# plot(totEmi_Ba$Group.1, totEmi_Ba$x, axes=FALSE, xlab = NA, ylab = NA, ylim = c(0,3500), pch=16, col = 'red')
+
+dev.off()
+
